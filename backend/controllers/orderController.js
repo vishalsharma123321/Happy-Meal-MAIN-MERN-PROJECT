28272c6b70f9e7886598f095b2edd0f941aa0bd3
Vishal Sharma <122232612+vishalsharma123321@users.noreply.github.com>
2025-09-04 19:40:22 +0530

    Update orderController.js replace the redirect in the order Controler with renders backend url

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -9,7 +9,8 @@ dotenv.config();
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const placeOrder = async (req, res) => {
-    const frontend_url = "http://localhost:5174";
+    // const frontend_url = "http://localhost:5174";
+     const frontend_url =  "https://happy-meal-back-end.onrender.com";
     try {
         const newOrder = new orderModel({
             userId: req.body.userId,
